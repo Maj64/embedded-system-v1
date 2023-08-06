286af936e75b46c23c59b18f7c90efd9ddcab018
Maj64 <manhnamhungdj1@gmail.com>
2023-08-06 23:25:57 +0700

    add: login page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import "normalize.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/global.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
